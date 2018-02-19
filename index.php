dfaa50d14959b64fe6cef2e2f4c799c83b38529d
martinsgc <32224222+martinsgc@users.noreply.github.com>
2018-02-19 11:12:19 -0300

    Criação de classe usuário

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+$root->loadById(9);
+echo $root;
+
 ?>
