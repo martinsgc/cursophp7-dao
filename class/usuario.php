370a136d3e12b77abc27284ab38c3f919d960ca5
martinsgc <32224222+martinsgc@users.noreply.github.com>
2018-02-20 08:23:42 -0300

    Métodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -59,5 +59,36 @@ class Usuario{
             "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
         ));
     }
+
+    public static function getList(){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", ARRAY(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public function login($login, $password){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+        if (count($results) > 0){
+            $row = $results[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+        else {
+            throw new Exception("Login e/ou senha inválido");
+        }
+
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,25 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuario();
+//Busca um usuário
+/*$root = new Usuario();
 $root->loadById(9);
 echo $root;
+*/
+
+//Carrega lista de usuários
+/*$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+//Carrega lista de usuários buscando por login
+/*$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+//Carrega usuário usando login e senha
+$usuario = new Usuario();
+$usuario->login("jose", "123456");
+echo $usuario;
 
 ?>
